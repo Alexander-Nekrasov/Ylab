43856fd933fb10d55f366de48d088e808a9cb079
Nekrasov Aleksandr <a.nekrasov@qa-academy.by>
2022-03-06 10:08:56 +0300

    Added changes to all classes

--- homework_2/additionalTask/Main.java
+++ homework_2/additionalTask/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         TicTac ticTac = new TicTac();
         ticTac.game();
+
         Results results = new Results();
     }
 }
--- homework_2/additionalTask/Results.java
+++ homework_2/additionalTask/Results.java
@@ -7,12 +7,12 @@ public class Results {
     static int countBot;
 
     Results() throws IOException {
-        readerFile = new BufferedReader(new FileReader("C:\\Users\\musoc\\Desktop\\JavaRush" +
-                "\\Ylab\\Ylab\\homework_2\\additionalTask\\results.txt"));
-        writerFile = new BufferedWriter(new FileWriter("C:\\Users\\musoc\\Desktop\\JavaRush" +
-                "\\Ylab\\Ylab\\homework_2\\additionalTask\\results.txt", true));
+        readerFile = new BufferedReader(new FileReader(getClass().getResource("/").getPath()
+                .substring(1).replace("out/production/Ylab","homework_2/additionalTask")+"results.txt"));
+        writerFile = new BufferedWriter(new FileWriter(getClass().getResource("/").getPath()
+                .substring(1).replace("out/production/Ylab","homework_2/additionalTask")+"results.txt", true));
 
-        if(readerFile.ready()) writerFile.write("\n");
+        if (readerFile.ready()) writerFile.write("\n");
         writerFile.write(textResults());
         writerFile.close();
     }
